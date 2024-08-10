fcfbf5536f8792fc30699013069b7eccc60911e4
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2024-08-10 16:55:09 -0400

    chore: install Nuxt UI module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-
+	modules: ["@nuxt/ui"],
 	future: {
 		compatibilityVersion: 4,
 	},
-
 	compatibilityDate: "2024-08-10",
 });
