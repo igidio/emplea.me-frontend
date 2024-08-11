a03e54c138e00cb637df4144d1ba7641c2966637
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2024-08-10 21:17:31 -0400

    feat(login): create basic structure

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,5 @@
 <template>
-	<div><NuxtRouteAnnouncer />sadsadsa</div>
+	<div>
+		<NuxtPage></NuxtPage>
+	</div>
 </template>
---
+++ app/pages/login.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<h3>Bienvenido de vuelta</h3>
+
+		<UCard>
+			<UInput
+				v-model="usernameOrEmail"
+				placeholder="Nombre de usuario o correo electrónico"
+			/>
+			<UInput v-model="password" placeholder="Contraseña" />
+			<UButton block>Iniciar sesión</UButton>
+			<a href="">¿Olvidaste tu contraseña?</a>
+			<UCheckbox v-model="rememberMe" name="remember-me" label="Recuérdame" />
+		</UCard>
+	</div>
+
+	<div>
+		También puedes probar:
+		<UButton>Ingresar con Google</UButton>
+		<UButton>Ingresar con LinkedIn</UButton>
+	</div>
+</template>
+
+<script setup lang="ts">
+const usernameOrEmail: Ref<string> = ref("");
+const password: Ref<string> = ref("");
+const rememberMe: Ref<boolean> = ref(true);
+</script>
