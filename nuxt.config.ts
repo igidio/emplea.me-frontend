a9a8f6405b12690aebfb3a68a300a7503d522483
Salvador Cáceres C. (HP Victus 15) <salvadorcaceresc@gmail.com>
2024-08-26 19:23:33 -0400

    feat: install graphql library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ["@nuxt/ui", "@nuxtjs/color-mode"],
+	modules: ["@nuxt/ui", "@nuxtjs/color-mode", "@nuxtjs/apollo"],
+	apollo: {
+		clients: {
+			default: {
+				httpEndpoint: "https://spacex-production.up.railway.app",
+			},
+		},
+	},
 	colorMode: {
 		preference: "light",
 		fallback: "light",
