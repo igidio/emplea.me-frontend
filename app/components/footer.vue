4b14b886623b43b9e35a9127b8bd162553dba272
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2024-09-08 23:33:55 -0400

    fix: add class to auth footer hyperlink

--- app/components/footer.vue
+++ app/components/footer.vue
@@ -1,7 +1,8 @@
 <template>
-	<footer class="h-auto text-center my-4 shrink-0">
+	<footer class="h-auto text-center py-4 shrink-0">
 		<span class="">
-			¿Aún no tienes una cuenta?, <a href="#">Regístrate, es gratis.</a>
+			¿Aún no tienes una cuenta?,
+			<a href="#" class="hyper">Regístrate, es gratis.</a>
 		</span>
 	</footer>
 </template>
