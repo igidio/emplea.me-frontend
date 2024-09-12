8ac5bfa6ce8f4e106672fb12a23b442266e55be6
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2024-09-12 09:57:47 -0400

    feat(auth): auth external buttons available in select component

--- app/components/Signup/Select.vue
+++ app/components/Signup/Select.vue
@@ -1,6 +1,12 @@
 <template>
 	<h3 class="mb-4">¿Qué vas a hacer hoy?</h3>
 	<div class="flex flex-col w-full max-w-[652px] gap-4">
+		<div class="flex flex-col tablet:flex-row gap-3 w-full">
+			<AuthExternal
+				text-prefix="Registrarse con "
+				v-if="Object.keys(data_oauth).length == 0"
+			/>
+		</div>
 		<UButton
 			color="primary"
 			variant="outline"
@@ -22,7 +28,7 @@
 <script setup lang="ts">
 import signupData from "~/data/signup.data.js";
 
-const { selection, modify_query_params } = useSignup();
+const { selection, modify_query_params, data_oauth } = useSignup();
 
 //const selection = defineModel();
 </script>
