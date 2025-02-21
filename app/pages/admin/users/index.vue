b0904bb83e51640ae47b81330c3fed37bd225404
Salvador Cáceres <salvadorcaceresc@gmail.com>
2025-02-21 13:25:14 -0400

    fix(admin): show correct quantity of logs on filter

--- app/pages/admin/users/index.vue
+++ app/pages/admin/users/index.vue
@@ -32,14 +32,62 @@
 			<template #header>Usuarios</template>
 			<div class="flex flex-col gap-4">
 				<div class="flex flex-col gap-2 justify-end w-64 self-end">
+					<UInput
+						icon="ri:search-2-line"
+						v-model="q"
+						placeholder="Filtrar resultados"
+						color="gray"
+						class="w-full"
+					/>
+					<USelectMenu
+						icon="ri:checkbox-multiple-line"
+						v-model="selectedColumns"
+						:options="columns"
+						multiple
+						placeholder="Columns"
+						aria-multiselectable="false"
+						color="gray"
+						size="md"
+						class="w-full"
+					>
+						<template #label>
+							{{ selectedColumns.length }} {{ selectedColumns.length > 1 ? 'seleccionados' : 'seleccionado' }}
+						</template>
+					</USelectMenu>
+					<USelect
+						color="gray"
+						size="md"
+						placeholder="Filtrar por estado"
+						icon="ri:filter-line"
+						v-model="search_by_status"
+						:options="[
+              {label: 'Todos', value: undefined},
+              {label: 'Activo', value: 'Activo'},
+              {label: 'Inactivo', value: 'Inactivo'},
+              {label: 'Sin verificar', value: 'Sin verificar'}
+          ]"
+					/>
+					<USelect
+						color="gray"
+						size="md"
+						placeholder="Filtrar por rol"
+						icon="ri:filter-line"
+						v-model="search_by_role"
+						:options="[
+              {label: 'Todos', value: undefined},
+              {label: 'Super usuario', value: 'SUPERUSER'},
+              {label: 'Administrador', value: 'ADMIN'},
+              {label: 'Empleador', value: 'EMPLOYER'},
+              {label: 'Aplicante', value: 'SEEKER'},
+          ]"
+					/>
 				</div>
 
 				<UTable
-					:rows="users"
+					:rows="rows.results"
 					:columns="selectedColumns"
 					:loading="loading"
 					:sort="{ column: 'created_at', direction: 'desc' }"
-					v-model:expand="expand"
 				>
 					<template #image-data="{row}">
 						<img
@@ -57,7 +105,7 @@
 					</template>
 					<template #actions-data="{row}">
 						<UDropdown :items="options(row)"
-							v-if="(
+						           v-if="(
 								user_role as any === 'SUPERUSER' &&
 								row.role_raw !== 'SUPERUSER'
 							) || (
@@ -71,6 +119,11 @@
 					</template>
 				</UTable>
 			</div>
+			<template #footer>
+				<div class="w-full flex flex-row justify-end gap-4">
+					<UPagination v-model="page" :page-count="pageCount" :total="rows.total" size="md" v-if="rows.total"/>
+				</div>
+			</template>
 		</UCard>
 	</div>
 
@@ -82,7 +135,7 @@ import type {UserInterface} from "~/interfaces";
 import {es_date} from "~/helpers/es_date";
 import type {TableColumn} from "#ui/types";
 import {message_schema} from "~/schemas";
-import {AdminRolesEnum, ClientRolesEnum, RolesEnum} from "~/enums/server/roles.enum";
+import {RolesEnum} from "~/enums/server/roles.enum";
 
 definePageMeta({
 	middleware: 'role',
@@ -143,37 +196,40 @@ const columns: TableColumn[] = [
 
 const q = ref('')
 const page = ref(1)
-const pageCount = 5
+const pageCount = 6
 const selectedColumns = ref([...columns])
 const search_by_status = ref(undefined)
+const search_by_role = ref(undefined)
 
-const expand = ref({
-	openedRows: [],
-	row: {}
-})
 
 const rows = computed(() => {
-	let results = [];
-	if (!q.value) {
-		results = users.value.slice((page.value - 1) * pageCount, (page.value) * pageCount)
-	}
+	let results = users.value;
 
-	if (q.value) results = users.value.filter((e: any) => {
-		return Object.values(e).slice((page.value - 1) * pageCount, (page.value) * pageCount).some((value) => {
-			return String(value).toLowerCase().includes(q.value.toLowerCase())
-		})
-	})
+	if (q.value) {
+		results = results.filter((e: any) => {
+			return Object.values(e).some((value) => {
+				return String(value).toLowerCase().includes(q.value.toLowerCase())
+			})
+		});
+	}
 
 	if (search_by_status.value) {
-		results = results.filter((e: any) => e.status.label.includes(search_by_status.value))
+		results = results.filter((e: any) => e.status.label.includes(search_by_status.value));
 	}
 
-	return results
-})
+	if (search_by_role.value) {
+		results = results.filter((e: any) => e.role_raw.includes(search_by_role.value));
+	}
+
+	return {
+		results: results.slice((page.value - 1) * pageCount, page.value * pageCount),
+		total: results.length
+	}
+});
 
 const options = (row: any) => [
 	[
-		...([{
+		{
 			label: (row.is_active) ? 'Deshabilitar' : 'Volver a habilitar',
 			icon: (row.is_active) ? 'ri:close-circle-line' : 'ri:arrow-up-circle-line',
 			click: () => {
@@ -181,12 +237,7 @@ const options = (row: any) => [
 				modal_data.value.header = (row.is_active) ? 'Deshabilitar usuario' : 'Volver a habilitar usuario'
 				modal_data.value.id = row.id
 			}
-		}]),
-		// {
-		// 	label: 'Editar',
-		// 	icon: 'ri:edit-line',
-		// 	click: () => useRouter().push(`/admin/posts/${row.id}`)
-		// }
+		},
 	]
 ]
 
